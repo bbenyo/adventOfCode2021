c5da88991e7404a30d324a733def4fd4eb47ab77
Brett Benyo <brett.benyo@raytheon.com>
2021-12-12 11:27:33 -0500

    Day 8 part 1

---
+++ src/bb/aoc2021/handler/Day8.java
@@ -0,0 +1,90 @@
+package bb.aoc2021.handler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import bb.aoc2021.InputHandler;
+
+public class Day8 implements InputHandler {
+	static private Logger logger = Logger.getLogger(Day8.class.getName());
+	
+	protected class Digits {
+		// Wire a -> could be (a,b,c,d), etc
+		Map<Character, List<Character>> wirePotentials = new HashMap<Character, List<Character>>();
+		
+		public Digits() {
+			for (char c = 'a'; c <= 'g'; ++c) {
+				List<Character> pots = new ArrayList<Character>();
+				wirePotentials.put(c, pots);
+				for (char p = 'a'; p <= 'g'; ++p) {
+					pots.add(p);
+				}
+			}
+		}
+	}
+	
+	protected List<Integer> mapOutput(String wiresOn) {
+		int len = wiresOn.length();
+		List<Integer> mapping = new ArrayList<Integer>();
+		switch (len) {
+		case 2 :
+			mapping.add(1);
+			break;
+		case 3 : 
+			mapping.add(7);
+			break;
+		case 4 : 
+			mapping.add(4);
+			break;
+		case 5 :
+			mapping.add(2);
+			mapping.add(3);
+			mapping.add(5);
+			break;
+		case 6 :
+			mapping.add(0);
+			mapping.add(6);
+			mapping.add(9);
+			break;
+		case 7 :
+			mapping.add(8);
+			break;
+		default :
+			logger.error("Improper length of wires in output: "+wiresOn);
+		}
+		return mapping;
+	}
+	
+	int knownDigitCount = 0;
+
+	@Override
+	public void handleInput(String line) {
+		String[] io = line.split("\\|");
+		if (io.length != 2) {	
+			logger.warn("Unable to parse input: "+line);
+			return;
+		}
+		
+		// Not necessary to parse for problem 1, we only handle the output!
+		// String inputValues = io[0];
+		
+		String outputValues = io[1].trim();
+		String[] outputDigits = outputValues.split(" ");
+		for (String oDigit : outputDigits) {
+			List<Integer> pValues = mapOutput(oDigit.trim());
+			if (pValues.size() == 1) {
+				knownDigitCount++;
+			}
+		}
+	}
+
+	@Override
+	public void output() {
+		logger.info("Known digit count: "+knownDigitCount);
+	}
+
+}
